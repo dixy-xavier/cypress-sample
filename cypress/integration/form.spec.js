31b05c14ad823b375ce3a949cf59ff09a8abd771
dixy-xavier <dixyxavier@gmail.com>
2021-01-29 16:56:37 +0100

    adds cypress test cases on a form

---
+++ cypress/integration/form.spec.js
@@ -0,0 +1,21 @@
+describe("Form tests", () => {
+  it("Can fill the form", () => {
+    cy.visit("/");
+    cy.get("form");
+    cy.get('input[name="name"]').type("Molly").should("have.value", "Molly");
+    cy.get('input[name="email"]')
+      .type("molly@dev.dev")
+      .should("have.value", "molly@dev.dev");
+    cy.get("textarea")
+      .type("Mind you if I ask some silly question?")
+      .should("have.value", "Mind you if I ask some silly question?");
+    cy.server();
+    cy.route({
+      url: "/users/**",
+      method: "POST",
+      response: { status: "Form Saved!", code: 201 },
+    });
+    cy.get("form").submit();
+    cy.contains("Form Saved!");
+  });
+});
